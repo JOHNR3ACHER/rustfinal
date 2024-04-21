8e474856cabdef2f92688e8d7cb69ef6b40df3f6
JesusGracia <100613686+JOHNR3ACHER@users.noreply.github.com>
2024-04-21 19:25:58 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,20 @@ use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
+use std::env;
+use std::fs;
+use std::fs::ReadDir;
+
+fn squential(paths:ReadDir){
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("/workspaces/rustfinal/books").unwrap();
+
+    squential(paths);
+
 }
