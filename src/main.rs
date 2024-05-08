0cb16cf7fb86cbd4807c0b650654f3b8768d7f49
JesusGracia <100613686+JOHNR3ACHER@users.noreply.github.com>
2024-05-08 23:30:53 +0000

    Completed

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate rayon;
 use rayon::prelude::*;
 use std::sync::mpsc::{channel, Sender};
 use std::sync::{Arc, Mutex};
-use std::thread;
 
 fn squential(paths: Vec<DirEntry>) -> HashMap<String, i32> {
     //Initalize new hashmap
@@ -59,33 +58,36 @@ enum Message {
     FileContents(Vec<String>),
     WordCounts(Vec<HashMap<String, i32>>),
     CombinedCount(HashMap<String, i32>),
-    Shutdown,
 }
 
+//Defines an actor that reads file contents and sends them as a message
 struct ReadActor {
     sender: Sender<Message>,
 }
 
-//sends
+
 impl ReadActor {
     //Function that reads contents of path
     fn read(&self, paths: Vec<DirEntry>) {
         let contents: Vec<String> = paths
             //uses parallel iterators
             .into_par_iter()
+            //reads contents of files and returns them as strings
             .map(|entry| fs::read_to_string(entry.path()).unwrap_or_default())
             .collect();
+        //sends a message of type FileContents
         self.sender.send(Message::FileContents(contents.clone())).unwrap();
     }
 }
 
+//Defines an actor that counts the file contents and sends them as a message
 struct CountActor {
     sender: Sender<Message>,
 }
 
 impl CountActor {
+    //function that counts the number of times a string has been seen in contents
     fn count(&self, words: Vec<String>) {
-        println!("CountActor received message");
         let wordcount: Vec<HashMap<String, i32>> = words
             //uses parallel iterators
             .into_par_iter()
@@ -100,19 +102,21 @@ impl CountActor {
                 wordcount
             })
             .collect();
-        println!("CountActor: Sending WordCounts message.");
+        //sends a message of type WordCount
         self.sender.send(Message::WordCounts(wordcount)).unwrap();
     }
 }
 
+//Defines an actor that combines the contents of multiple maps and sends them as a message
 struct CombineActor {
     sender: Sender<Message>,
 }
 
 impl CombineActor {
+    //function that combines all HashMaps into One 
     fn combine(&self, hashs: Vec<HashMap<String, i32>>) {
-        println!("CombineActor received message");
         let combined: HashMap<String, i32> =
+            //folds all Hashs into one accumulator
             hashs.into_iter().fold(HashMap::new(), |mut acc, map| {
                 //loops through all contents of map
                 for (word, count) in map {
@@ -121,58 +125,50 @@ impl CombineActor {
                 }
                 acc
             });
-        println!("CombineActor: Sending CombinedCount message.");
+            //sends a message of type CombinedCount
         self.sender.send(Message::CombinedCount(combined)).unwrap();
     }
 }
 
 fn pipelineparalelism(paths: Vec<DirEntry>) -> HashMap<String, i32> {
-    //println!("Starting pipelineparalelism...");
+    //Creates a communication between threads linking sender and receiver
     let (sender, receiver) = channel();
 
+    //initializes all actors
     let read = ReadActor {sender: sender.clone(),};
     let count = CountActor {sender: sender.clone(),};
     let combine = CombineActor {sender: sender.clone(),};
 
+    //Creates a cloneable receiver
     let receiver = Arc::new(Mutex::new(receiver));
 
-    let receiverclone = receiver.clone();
+    //calls read function of ReadActor
+    read.read(paths);
 
-    thread::spawn(move || read.read(paths));
+    //checks reeiver of it has a message of type FileContents with data filecontents if so set words to filecontents
+    let words = match receiver.lock().unwrap().recv() {
+        Ok(Message::FileContents(filecontents)) => filecontents,
+        _ =>  return HashMap::new(),
+    };
 
-    thread::spawn(move || {
-        println!("CountActor before receiving message"); // Debug print
-        let words = match receiverclone.lock().unwrap().recv() {
-            Ok(Message::FileContents(contents)) => contents,
-            _ => {
-                println!("Error: Failed to receive FileContents message");
-                return;
-            },
-        };
-        println!("CountActor after receiving message"); // Debug print
-        count.count(words);
-    });
+    //calls count function of CountActor
+    count.count(words);
 
-    let receiverclone = receiver.clone();
-    thread::spawn(move || {
-        println!("CombineActor before receiving message"); // Debug print
-        let count = match receiverclone.lock().unwrap().recv() {
-            Ok(Message::WordCounts(counts)) => counts,
-            _ => {
-                println!("Error: Failed to receive WordCounts message");
-                return;
-            },
-        };
-        println!("CombineActor after receiving message"); // Debug print
-        combine.combine(count);
-    });
+    //checks reeiver of it has a message of type WordCounts with data count if so set count to count
+    let count = match receiver.lock().unwrap().recv() {
+        Ok(Message::WordCounts(count)) => count,
+        _ => return HashMap::new(),
+    };
 
-    // Receive the final combined word count
-    let x = match receiver.lock().unwrap().recv() {
-        Ok(Message::CombinedCount(combined_word_count)) => combined_word_count,
+    //calls combine function of CombineActor
+    combine.combine(count);
+
+   //checks reeiver of it has a message of type CombinedCount with data combined_count if so return combined_count
+    return match receiver.lock().unwrap().recv() {
+        Ok(Message::CombinedCount(combined_count)) => combined_count,
         _ => HashMap::new(),
     };
-    x
+
 }
 
 fn main() {
